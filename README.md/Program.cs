0c2fb6c495e086cb1747f254e00f69d55bef7b63
hobao-133 <kevinbh133@gmail.com>
2024-08-09 17:35:38 +0700

    done

---
+++ README.md/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Thuchanhngoaite;
+class Program{
+    static void Main(string [] args){
+        //console.Writeline("Currency Exchange");
+        int vnd;
+        int usd;
+        Console.WriteLine("Enter amount of usd");
+        usd = Int32.Parse(Console.ReadLine());
+        Console.WriteLine(vnd = usd * 23000);
+    }
+}
